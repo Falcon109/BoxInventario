97ba82525dd607e1983fa90097be6435c4ec5fa9
Falcon_109 <benja.fuente123@gmail.com>
2023-04-02 03:31:39 -0400

    Segunda y Tercera Ventana

---
+++ app/src/main/java/com/example/boxinventario/CreacionLista.java
@@ -0,0 +1,14 @@
+package com.example.boxinventario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreacionLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creacion_lista);
+    }
+}
